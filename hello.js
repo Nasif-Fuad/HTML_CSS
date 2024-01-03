803c9c1455b6a9b465711fdb297069c5ca2f64aa
Nasif Fuad <nasifuad91@gmail.com>
2024-01-03 09:55:27 +0600

    My Excercises

---
+++ hello.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+let x=100
+let v=11
+y="nasif"
+z=x+y
+console.log("age is "+x,"name is "+y,"combined "+z)
+c=typeof(z)
+console.log(c)
+const person ={firstname:"nasif",lastname:"dope"}
+const cars=["volvo","farari","bugati"]
+const date=new Date("2020-12-02")
+console.log(date)
